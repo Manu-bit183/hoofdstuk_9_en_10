5f6c18847a3814109ffa0f78688cbddf81861f26
MPal <118168603+Manu-bit183@users.noreply.github.com>
2023-10-10 12:21:25 +0200

    Made changes on the url.

--- index.php
+++ index.php
@@ -1,23 +1,31 @@
 <?php
-try {
-    $fietsenmaker = new PDO("mysql:host=localhost;dbname=fietsenmaker","root","");
-    $query = db->prepare("select * FROM fietsen");
-    $query->execute();
-} catch(PDOExeception $e) {
-    die("Error!: " . $e->getMessage());
-}
 
 try {
-    $sql = "INSERT INTO `fietsen` (`id`, `merk`, `type`, `prijs`) VALUES (\'1234\', \'batavus\\r\\nbatavus\\r\\ngazelle\\r\\ngazelle\\r\\ngazelle\\r\\ngiant\\r\\ngiant\', \'blockbuster\\r\\nflying D\\r\\neclipse\\r\\ngiro\\r\\ncompetition\\r\\nexpedition AT\', \'699\\r\\n749\\r\\n1049\\r\\n799\\r\\n899\\r\\n999\\r\\n1299\')";
+    $sql = INSERT INTO `fietsen`(`id`, `merk`, `type`, `prijs`)
+VALUES(
+    '1234',
+    'batavus',
+    'blockbuster',
+    '699'
+),('4326', 'batavus', 'flying D', '749'),('7544', 'gazelle', 'eclipse', '799'),('8643', 'gazelle', 'giro', '899'),('8965', 'giant', 'competition', '999'),(
+    '5689',
+    'giant',
+    'expedition AT',
+    '1.299'
+);
     $fietsenmaker = new PDO("mysql:host=localhost;dbname=fietsenmaker","root","");
     $query = db->prepare("select * FROM fietsen");
     $query->execute();
     $result = $query-fetchAll(PDO::FETCH_ASSOC);
-    foreach($result as &$data) {
-        echo $data["merk"] . "";
-        echo $data["type"] . "";
-        echo $data["prijs"] . "<br>";
+    echo "</table>";
+    foreach($result as &$sql) {
+        echo "</td>";
+            echo "</td>"; $sql["merk"] . "</td>";
+            echo "</td>"; $sql["type"] . "</td>";
+            echo "</td>"; $sql["prijs"] . "</td>";
+        echo "</td>";
     }
+    echo "</table>";
 } catch(PDOExeception $e) {
     die("Error!: " . $e->getMessage());
 }
