3d30cd4e57f1a90736f093d8ccd7612f55167cf1
MPal <118168603+Manu-bit183@users.noreply.github.com>
2023-10-10 11:40:58 +0200

    have made a progress
    have made a progress

--- index.php
+++ index.php
@@ -7,4 +7,19 @@ try {
     die("Error!: " . $e->getMessage());
 }
 
+try {
+    $db = new PDO("mysql:host=localhost;dbname=fietsenmaker","root","");
+    $query = db->prepare("select * FROM fietsen");
+    $query->execute();
+    $result = $query-fetchAll(PDO::FETCH_ASSOC);
+    foreach($result as &$data) {
+        echo $data["merk"] . "";
+        echo $data["type"] . "";
+        echo $data["prijs"] . "<br>";
+    }
+} catch(PDOExeception $e) {
+    die("Error!: " . $e->getMessage());
+}
+
+
 ?>
