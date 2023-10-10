e2586caebf24ff395d788a88603f71a56b4f7ae2
MPal <118168603+Manu-bit183@users.noreply.github.com>
2023-10-10 12:01:40 +0200

    Made changes on the url.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $db = new PDO("mysql:host=localhost;dbname=fietsenmaker","root","");
+    $fietsenmaker = new PDO("mysql:host=localhost;dbname=fietsenmaker","root","");
     $query = db->prepare("select * FROM fietsen");
     $query->execute();
 } catch(PDOExeception $e) {
@@ -8,7 +8,8 @@ try {
 }
 
 try {
-    $db = new PDO("mysql:host=localhost;dbname=fietsenmaker","root","");
+    $sql = "INSERT INTO `fietsen` (`id`, `merk`, `type`, `prijs`) VALUES (\'1234\', \'batavus\\r\\nbatavus\\r\\ngazelle\\r\\ngazelle\\r\\ngazelle\\r\\ngiant\\r\\ngiant\', \'blockbuster\\r\\nflying D\\r\\neclipse\\r\\ngiro\\r\\ncompetition\\r\\nexpedition AT\', \'699\\r\\n749\\r\\n1049\\r\\n799\\r\\n899\\r\\n999\\r\\n1299\')";
+    $fietsenmaker = new PDO("mysql:host=localhost;dbname=fietsenmaker","root","");
     $query = db->prepare("select * FROM fietsen");
     $query->execute();
     $result = $query-fetchAll(PDO::FETCH_ASSOC);
