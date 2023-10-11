6d5eb46a88f601b4ffe4bdf7916a8a0295c2b468
MPal <118168603+Manu-bit183@users.noreply.github.com>
2023-10-11 09:00:56 +0200

    have finished
    Have made 1,2,3 en 4.

--- index.php
+++ index.php
@@ -1,34 +1,37 @@
 <?php
-
 try {
-    $sql = INSERT INTO `fietsen`(`id`, `merk`, `type`, `prijs`)
-VALUES(
-    '1234',
-    'batavus',
-    'blockbuster',
-    '699'
-),('4326', 'batavus', 'flying D', '749'),('7544', 'gazelle', 'eclipse', '799'),('8643', 'gazelle', 'giro', '899'),('8965', 'giant', 'competition', '999'),(
-    '5689',
-    'giant',
-    'expedition AT',
-    '1.299'
-);
-    $fietsenmaker = new PDO("mysql:host=localhost;dbname=fietsenmaker","root","");
-    $query = db->prepare("select * FROM fietsen");
+    $db = new PDO("mysql:host=localhost;dbname=opdracht3",
+    "root", "");
+
+    $query = $db->prepare("SELECT * FROM cijfers");
     $query->execute();
-    $result = $query-fetchAll(PDO::FETCH_ASSOC);
-    echo "</table>";
-    foreach($result as &$sql) {
-        echo "</td>";
-            echo "</td>"; $sql["merk"] . "</td>";
-            echo "</td>"; $sql["type"] . "</td>";
-            echo "</td>"; $sql["prijs"] . "</td>";
-        echo "</td>";
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Leerling</th>";
+    echo "<th>Cijfer</th>";
+    echo "</tr>";
+    foreach ($result as &$data){
+        echo"<tr>";
+        echo "<td>". $data["id"] . "</td>";
+        echo "<td>". $data["leerling"] . "</td>";
+        echo "<td>". $data["cijfer"] . "</td>";
+        echo "</tr>";
     }
-    echo "</table>";
-} catch(PDOExeception $e) {
-    die("Error!: " . $e->getMessage());
+echo "</table>";
+}catch (PDOException $e){
+    die("Error!" . $e->getMessage());
 }
 
-
 ?>
+
+<style type="text/css">
+    table {
+        border-collapse: collapse;
+        border: 1px solid black;
+    }
+    td{
+        border: 1px solid black;
+        width: 100px;
+    }
+</style>
